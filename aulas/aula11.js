abc2ec5d19decfbe838ff42a75906c8f135b9cab
Eduardo Milano Dantas <eduardomilanodantas@gmail.com>
2025-10-25 12:16:25 -0300

    a

---
+++ aulas/aula11.js
@@ -0,0 +1,7 @@
+const dc1 = document.getElementById("c1");
+console.log(dc1);
+
+const colecaoHTML = document.getElementsByTagName("div");
+console.log(colecaoHTML);
+
+const colecaoArray = document.getElementsByClassName("cc");

54047cc3c58937f270dfe39d1d6a80d77d0a6854
Eduardo Milano Dantas <eduardomilanodantas@gmail.com>
2025-10-29 20:21:24 -0300

    Commit

---
+++ aulas/aula19/aula19.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let n1 = parseInt(window.prompt("Digite um número:"))
+let n2 = parseInt(window.prompt("Digite outro número:"))
+let s = n1 + n2;
+let m = n1 - n2;
+let v = n1 * n2;
+let d = n1 / n2;
+
+alert("A soma é: " + s);
+alert("A subtração é: " + m);
+alert("A multiplicação é: " + v);
+alert('A divisão è ' + d);
+alert("E a média aritmética dos números são: " + s / 2);
